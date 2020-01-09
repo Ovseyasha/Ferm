0d66414b7af61b369a507badb7738e070258d62d
Ovchinnikov Alexandr <59626341+Ovseyasha@users.noreply.github.com>
2020-01-10 00:10:25 +0300

    Add new file

---
+++ main.php
@@ -0,0 +1,98 @@
+<?php
+  class Cow {
+    public $id;
+    public $product;
+
+    function __construct($id,$product)
+    {
+      $this->id = $id;
+      $this->product = $product;
+    }
+  }
+  class Chicken{
+    public $id;
+    public $product;
+
+    function __construct($id,$product)
+    {
+      $this->id = $id;
+      $this->product = $product;
+    }
+  }
+	function addAnimal ($countCow,$countChicken){
+
+		//Начальное количество животных
+		echo "\nВсего коров $countCow\n";
+		echo "Всего куриц $countChicken\n\n";
+
+		//Добавление животных
+			$addCow = readline("Сколько добавить коров: ");
+			$addChicken = readline("Сколько добавить куриц: ");
+
+				if (is_numeric($addCow) and is_numeric($addChicken)){
+					global $newCountCow;
+					global $newCountChicken;
+					$newCountCow = $countCow + $addCow;
+					$newCountChicken = $countChicken + $addChicken;
+				}else  {
+					echo "Введите число!\n";
+					addAnimal(10,20);
+				}
+
+	}
+	function collect($newCountCow,$newCountChicken){
+		$CowArr = array ();
+
+		//добавление коров
+		for ($i = 1; $i <= $newCountCow; $i++){
+			array_push($CowArr, new Cow ($i,rand(8,12)));
+		}
+
+		//добавление куриц
+		$chickenArr = array ();
+		for ($i = 1; $i <= $newCountChicken; $i++){
+			array_push($chickenArr, new Chicken ($i,rand(0,1)));
+		}
+
+		echo "\nВсего коров $newCountCow\n";
+		echo "Всего куриц $newCountChicken\n";
+		//Подсчет литров
+		foreach ($CowArr as $item ) {
+			 global $sumMilk;
+			 $sumMilk = $sumMilk + $item->product;
+		}
+		echo "\nМолока собранно - ".$sumMilk." литров\n";
+
+		//Подсчет яиц
+		foreach ($chickenArr as $item ) {
+				global $sumEggs;
+				$sumEggs = $sumEggs + $item->product;
+		}
+		echo "Яиц собранно - ".$sumEggs." штук\n";
+	}
+
+	addAnimal(10,20);
+	//Обновление количества животных
+	echo "\nКоличество коров - ".$newCountCow."\n";
+	echo 'Количество куриц - '.	$newCountChicken."\n\n";
+
+	//Сбор
+	$d = 0;
+	while ($d != 1) {
+		$collect = readline ("Cобрать продукцию - collect, Изменить ко-во жив-ых - edit, Выйти - exit: ");
+		if ($collect == 'collect'){
+			collect($newCountCow,$newCountChicken);
+
+			$allMilk = $allMilk + $sumMilk;
+			$allEggs = $allEggs + $sumEggs;
+			echo "\nВсего собранно ".$allMilk." литров молока и ".$allEggs." яиц\n\n";
+		}else if ($collect == 'exit'){
+			Exit();
+		}else if ($collect == 'edit'){
+					addAnimal($newCountCow,$newCountChicken);
+				}
+		else if ($collect != 'yes'){
+			echo "Ошибка! Неправильно набранна команда!\n";
+		}
+	}
+?>
